80d67f1c323450c2b86297ac4cfd7d2980f3da93
Arnab Deb <65126698+ARNAB131@users.noreply.github.com>
2025-09-03 14:53:35 +0000

    mark BookingPage as client and disable prerender for home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import "./globals.css";
-import BookingPage from "./BookingPage"; // adjust if you place it elsewhere
+import BookingPage from "./BookingPage";
 
 export default function Home() {
   return <BookingPage />;
